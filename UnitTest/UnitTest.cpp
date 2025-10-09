575de94dd340fd6b18d2965e5f89d43b14f23512
lpavel <lpavel@GCFRPA045>
2025-10-09 11:32:30 +0200

    first push

---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace MathsTests
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(Test)
+		{
+			
+		}
+	};
+}
