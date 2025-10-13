03292141aa78ae74c37474cfb4b532aa7b6cfa75
mcastelnot <mcastelnot@GCFRPA045>
2025-10-13 09:47:12 +0200

    préparation à la page inl

--- MathsLibrary/Quaternion.h
+++ MathsLibrary/Quaternion.h
@@ -10,33 +10,35 @@ public:
     // properties
 
     T EulerAngles(const Vec3& rhs){}
-    Vec4 Normalized() const { float magnitude = std::sqrt(x * x + y * y + z * z + w * w); if (magnitude < 0.00001f) { return Vec4(0.0f, 0.0f, 0.0f, 0.0f); } return Vec4(x / magnitude, y / magnitude, z / magnitude), w / magnitude; }
+    Vec4 Normalized() const; { float magnitude = std::sqrt(x * x + y * y + z * z + w * w); if (magnitude < 0.00001f) { return Vec4(0.0f, 0.0f, 0.0f, 0.0f); } return Vec4(x / magnitude, y / magnitude, z / magnitude), w / magnitude; }
 
 
     //public methods
 
-    bool Equals(const Vec4& rhs) const { return (x == rhs.x) && (y == rhs.y) && (z == rhs.z) && (w == rhs.w); }
-    void Set(const Vec4& rhs) { return  x = rhs.x, y = rhs.y, z = rhs.z, w = rhs.w; }
-    
+    bool Equals(const Vec4& rhs) const /*{ return (x == rhs.x) && (y == rhs.y) && (z == rhs.z) && (w == rhs.w); }*/
+    void Set(const Vec4& rhs); /*{ return  x = rhs.x, y = rhs.y, z = rhs.z, w = rhs.w; }*/
+
     T SetFromToRotation(const Vec3& fromDirection, const Vec3& toDirection){}
     T SetLookRotation(const Vec3& view, Vec3 up){}
     T ToAngleAxis(){}
-    std::string ToString() const { return "(" + std::to_string(x) + ", " + std::to_string(y) + ", " + std::to_string(z) + ", " + std::to_string(w) + ")"; }
+    std::string ToString() const; /* { return "(" + std::to_string(x) + ", " + std::to_string(y) + ", " + std::to_string(z) + ", " + std::to_string(w) + ")"; }*/
+
 
     // static methods
 
     T Euler(){}
     T LookRotation(){}
 
-    Vec4 Normalize(const Vec4& rhs) const {
-        float magnitude = std::sqrt(x * x + y * y + z * z + w * w);
-        
-        if (magnitude < 0.00001f) {
-            return Vec4(0.0f, 0.0f, 0.0f, 0.0f);
-        }
+    Vec4 Normalize(const Vec4& rhs) const;
+    //{
+    //    float magnitude = std::sqrt(x * x + y * y + z * z + w * w);
+    //    
+    //    if (magnitude < 0.00001f) {
+    //        return Vec4(0.0f, 0.0f, 0.0f, 0.0f);
+    //    }
 
-        return Vec4(x/ magnitude, y/ magnitude, z/ magnitude, w/ magnitude);
-    }
+    //    return Vec4(x/ magnitude, y/ magnitude, z/ magnitude, w/ magnitude);
+    //}
 
     T RotateTowards(){}
     T Slerp(){}
