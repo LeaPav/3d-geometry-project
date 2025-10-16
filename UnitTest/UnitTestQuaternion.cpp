9e3dbca4630a6a193945ac9a77da173c68e24679
Maëlyne <uchunookoku@gmail.com>
2025-10-16 21:23:27 +0200

    unitTest quaternion beginning

---
+++ UnitTest/UnitTestQuaternion.cpp
@@ -0,0 +1,29 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "Quaternion.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestQuaternion {
+	TEST_CLASS(Quaternion) {
+	public:
+		TEST_METHOD(){}
+		TEST_METHOD(){}
+		TEST_METHOD(){}
+		TEST_METHOD(){}
+		TEST_METHOD(){}
+		TEST_METHOD(){}
+		TEST_METHOD(Static_Angle){}
+		TEST_METHOD(Static_AngleAxis){}
+		TEST_METHOD(Static_Dot){}
+		TEST_METHOD(Static_Euler){}
+		TEST_METHOD(Static_FromToRotation){}
+		TEST_METHOD(Static_Inverse){}
+		TEST_METHOD(Static_Lerp){}
+		TEST_METHOD(Static_LerpUncampled){}
+		TEST_METHOD(Static_Normalize){}
+		TEST_METHOD(Static_RotateTowards){}
+		TEST_METHOD(Static_Slerp){}
+		TEST_METHOD(Static_SlerpUncampled){}
+	};
+}
