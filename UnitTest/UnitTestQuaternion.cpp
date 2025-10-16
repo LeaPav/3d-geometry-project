46e0306c222f12e05b5cd8de5f5b66bb1057f68b
Maëlyne <uchunookoku@gmail.com>
2025-10-16 21:36:21 +0200

    préparation des unitTest quaternion

--- UnitTest/UnitTestQuaternion.cpp
+++ UnitTest/UnitTestQuaternion.cpp
@@ -7,12 +7,21 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 namespace UnitTestQuaternion {
 	TEST_CLASS(Quaternion) {
 	public:
-		TEST_METHOD(){}
-		TEST_METHOD(){}
-		TEST_METHOD(){}
-		TEST_METHOD(){}
-		TEST_METHOD(){}
-		TEST_METHOD(){}
+
+		TEST_METHOD(Constructor_Default){}
+		TEST_METHOD(Constructor_Values){}
+		TEST_METHOD(Operator_Addition){}
+		TEST_METHOD(Operator_Equality) {}
+		//TEST_METHOD(Method_Identity) {}
+		//TEST_METHOD(Property_EulerAngles) {}
+		//TEST_METHOD(Property_Normalized) {}
+		TEST_METHOD(Operator_this) {}
+		TEST_METHOD(Method_Equals){}
+		TEST_METHOD(Method_Set){}
+		TEST_METHOD(Method_SetFromToRotation){}
+		TEST_METHOD(Method_SetLookRotation){}
+		TEST_METHOD(Method_ToAngleAxis){}
+		TEST_METHOD(Method_ToString){}
 		TEST_METHOD(Static_Angle){}
 		TEST_METHOD(Static_AngleAxis){}
 		TEST_METHOD(Static_Dot){}
